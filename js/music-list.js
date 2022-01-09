80dc9aff3c944b09dca92cedb83377ea1012f79f
Ayushop0 <97185581+Ayushop0@users.noreply.github.com>
2022-01-09 11:04:58 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,86 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Rap God",
+    artist: "Eninem",
+    img: "rapgod",
+    src: "rapgod"
+    },
+   {
+    name: "Bad Guy",
+    artist: "Billie Eilish",
+    img: "bad guy",
+    src: "bad guy"
+    },
+   {
+    name: "Industry Baby",
+    artist: "Jack Harlow, Lil Nas X",
+    img: "industry baby",
+    src: "industry baby"
+    },
+   {
+    name: "Money",
+    artist: "Lisa",
+    img: "money",
+    src: "money"
+    },
+   {
+   name: "Close Eyes",
+   artist: "DVRST",
+   img: "close,eyes",
+   src: "close,eyes"
+   },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
